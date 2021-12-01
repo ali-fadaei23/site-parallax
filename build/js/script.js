67800121f7d3db0333107f8a9531372ce563aeb8
ali-fadaei23 <64379649+ali-fadaei23@users.noreply.github.com>
2021-12-01 21:24:57 +0330

    element in viewport

--- build/js/script.js
+++ build/js/script.js
@@ -3,11 +3,23 @@ $(function () {
   let headerCue = document.querySelector(".header-cue");
   let nav = document.querySelector(".site-nav");
   let gamesWitcher = document.querySelector("#games");
+  let witcher = document.querySelector("#witcher-2");
   let navHeight = nav.scrollHeight;
+
+  function inViewport(el) {
+    const rect = el.getBoundingClientRect();
+    return (
+      (rect.top <= 0 && rect.bottom >= 0) ||
+      (rect.bottom >= window.innerHeight && rect.top <= window.innerHeight) ||
+      (rect.top >= 0 && rect.bottom <= window.innerHeight)
+    );
+  }
 
   function moveHandler() {
     let top = window.pageYOffset;
     let mainOnTop = gamesWitcher.getBoundingClientRect().top - navHeight;
+
+    console.log(inViewport(witcher));
 
     mainOnTop < 0
       ? nav.classList.add("in-body")
@@ -19,7 +31,7 @@ $(function () {
       ? headerCue.classList.add("d-none")
       : headerCue.classList.remove("d-none");
 
-    headerContent.style.transform = `translateY(-${top / 1.5}px)`;
+    headerContent.style.transform = `translateY(-${top / 2}px)`;
     headerContent.style.opacity =
       1 - Math.max(top / (window.innerHeight * 0.2), 0);
 
